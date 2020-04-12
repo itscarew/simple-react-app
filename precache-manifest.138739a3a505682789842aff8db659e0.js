7f9761bbf491cc64aaab508e676d7bdbda7ac270
itscarew <itscarew@gmail.com>
2020-04-12 21:27:02 +0100

    Updates

---
+++ precache-manifest.138739a3a505682789842aff8db659e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ca8d489e059a087caee0f5988827ec8",
+    "url": "/simple-react-app/index.html"
+  },
+  {
+    "revision": "b02222d96acb8d7f7ce2",
+    "url": "/simple-react-app/static/css/main.82bc8f3f.chunk.css"
+  },
+  {
+    "revision": "9bc63b16c58290c6be7c",
+    "url": "/simple-react-app/static/js/2.11d15f81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-app/static/js/2.11d15f81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b02222d96acb8d7f7ce2",
+    "url": "/simple-react-app/static/js/main.95763c67.chunk.js"
+  },
+  {
+    "revision": "d51ddeb86ea0d58ef0ee",
+    "url": "/simple-react-app/static/js/runtime-main.3fa695a1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/simple-react-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
